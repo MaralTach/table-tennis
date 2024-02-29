3c179c3b6515e3337d1163a4b7e3942ddeb771ae
MaralTach <yoloten.maral@gmail.com>
2024-02-29 19:11:01 +0300

    update

--- script.js
+++ script.js
@@ -10,6 +10,11 @@ let winScore = 5;
 let gameOver = false;
 input.focus()
 
+input.addEventListener('change', function() {
+    winScore = parseInt(this.value);
+    resetGame();
+});
+
 player1Btn.addEventListener('click', (e) =>{
     // alert('player1')
     // const tahmin = input.value ;
@@ -22,11 +27,12 @@ player1Btn.addEventListener('click', (e) =>{
         gameOver = true;
         p1.style.color = "green";
         p2.style.color = "red";
-
         player1Btn.disabled = true;   
         player2Btn.disabled = true; 
+        
     }
     }
+    
 })
 
 
@@ -34,26 +40,39 @@ player1Btn.addEventListener('click', (e) =>{
 player2Btn.addEventListener('click', (e) =>{
     // alert('player2')
     if(!gameOver){
-   
+        
         let score = Number(p2.textContent) + 1
           p2.textContent = score
    
           if (score >= winScore) {
            gameOver = true;
+           
            p2.style.color = "green";
            p1.style.color = "red";
-       player1Btn.disabled = true;   
-       player2Btn.disabled = true;   
+           player1Btn.disabled = true;   
+           player2Btn.disabled = true; 
+           return alert('Player 2 Win')
+           
        }
-       }
+    }
 })
 
 resetBtn.addEventListener('click', function (){
    p1.textContent = '0'
    p2.textContent = '0'
    
-  
+   resetGame() 
 })
 
 
+
+function resetGame() {
+    p1.textContent = '0';
+    p2.textContent = '0';
+    gameOver = false;
+    p1.style.color = 'black';
+    p2.style.color = 'black';
+    player1Btn.disabled = false;
+    player2Btn.disabled = false;
+}
 
