9c796b8d6e124aa2b6e66e78ddfafb289fd67ff6
MaralTach <yoloten.maral@gmail.com>
2024-02-29 15:34:11 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,59 @@
+const player1Btn = document.getElementById('player1');
+const player2Btn = document.getElementById('player2');
+const resetBtn = document.getElementById('reset');
+const input = document.getElementById('winningtable');
+const p1 = document.querySelector('#p1')
+const p2 = document.querySelector('#p2')
+
+// let score = 5
+let winScore = 5;
+let gameOver = false;
+input.focus()
+
+player1Btn.addEventListener('click', (e) =>{
+    // alert('player1')
+    // const tahmin = input.value ;
+    if(!gameOver){
+   
+     let score = Number(p1.textContent) + 1
+       p1.textContent = score
+
+       if (score >= winScore) {
+        gameOver = true;
+        p1.style.color = "green";
+        p2.style.color = "red";
+
+        player1Btn.disabled = true;   
+        player2Btn.disabled = true; 
+    }
+    }
+})
+
+
+
+player2Btn.addEventListener('click', (e) =>{
+    // alert('player2')
+    if(!gameOver){
+   
+        let score = Number(p2.textContent) + 1
+          p2.textContent = score
+   
+          if (score >= winScore) {
+           gameOver = true;
+           p2.style.color = "green";
+           p1.style.color = "red";
+       player1Btn.disabled = true;   
+       player2Btn.disabled = true;   
+       }
+       }
+})
+
+resetBtn.addEventListener('click', function (){
+   p1.textContent = '0'
+   p2.textContent = '0'
+   
+  
+})
+
+
+
